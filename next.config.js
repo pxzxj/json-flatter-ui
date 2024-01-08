c32024c37061e58946790ebae26c2479f9a369dd
pxzxj <pudge.zxj@gmail.com>
2024-01-08 20:40:19 +0800

    完成一部分功能

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {output: 'export',}
+const nextConfig = {
+    basePath: '/json-flatter-ui',
+    output: 'export'
+}
 
 module.exports = nextConfig
